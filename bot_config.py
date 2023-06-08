e0e7707fb24b9d9ca2537f38db3ea99f797f8b83
maubers <126840418+maubers@users.noreply.github.com>
2023-06-08 13:43:35 -0400

    Shutup mention added
    added phrase to get her to ignore a comment, aka shut the fuck up

--- bot_config.py
+++ bot_config.py
@@ -9,7 +9,7 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import torch
 
 # Load GPT-2 model and tokenizer
-model_path = "emily_yeppers_3.0"
+model_path = "emily_yeppers_4.0"
 tokenizer = GPT2Tokenizer.from_pretrained(model_path)
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.pad_token_id = tokenizer.eos_token_id
@@ -17,7 +17,7 @@ model = GPT2LMHeadModel.from_pretrained(model_path, pad_token_id=tokenizer.pad_t
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-crazy_factor = 1.2      # Keep high if you want her to act crazy
+crazy_factor = 1.3      # Keep high if you want her to act crazy
 response_length = 150   # She will rant about cum forever at a higher temperature if you don't keep this lower
 
 # Define list of words that neither prompts nor replies should contain
@@ -35,7 +35,8 @@ password = os.environ["PASSWORD"]
 
 
 # Set the target phrase, subreddit, and user relationships
-TARGET_PHRASES = ['emily yeppers', 'cum will win', 'olive juice']
-subreddits = ['shittyaskreddit', 'AssCredit']
+TARGET_PHRASES = ['emily yeppers', ' cum ', 'olive juice']
+subreddits = ['shittyaskreddit', 'testthewalrus', 'AssCredit']
 bot_mention = "u/EMILY\_YEPPERS"
+shutup_mention = "shut the fuck up"
 relationships = {}      # Use this dictionary to make the bot behave differently with specific users
