aeec9646e27c8930e8b6662e063fe08ff9c9c673
maubers <126840418+maubers@users.noreply.github.com>
2023-06-08 15:55:09 -0400

    Fixes occasional lack of space after punctuation & error generated from similarity score function
    Was having a hard time producing a response because similarity_score didn't like newlines. Now it's fine with them.
    Also fixes the new issue where spaces weren't always included after punctuation
    every time I fix a problem, I create another one

--- response_handler.py
+++ response_handler.py
@@ -64,29 +64,24 @@ def create_gpt_response(prompt):
 
 
 def format_response(prompt, response):
-
     if response:
-        ### THE WONDERFUL SECTION WITH REGEX PATTERNS. GOD I HATE REGEX PATTERNS
-        
-        # Replace this weird header thing that I can only guess originates from quotes if it appears
+        # Replace headers
         pattern = r"\w+\s\d{2}/\d{2}/\d{2}\s\((?:Mon|Tue|Wed|Thu|Fri|Sat|Sun)\)\s\d{2}:\d{2}:\d{2}\s(?:AM|PM)\sNo\.\s\d+(?:\s>>\d+)?"
         response = re.sub(pattern, '', response)
-        print(f"First regex pattern check complete (post date/time header). New response: {response}")
         
-        # Replace this gif link shit. It doesn't work. I gave up.
+        # Replace gif links
         pattern = (r"gif\]\((giphy\|\w+)\)")
         match = re.sub(pattern, '', response)
-        print(f"Second regex pattern check complete (gif links). New response: {response}")
         
         # Replace any urls
         response = re.sub(r'http\S+', '', response)
-        print(f"Third regex pattern check complete (regular ol' hyperlinks). New response: {response}")
+
+        # Add space after any punctuation if there isn't a space already
+        response = re.sub(r'([.!?])([^\s])', r'\1 \2', response)
         
         # Split text into sentences
         sentences = [s.strip() for s in re.split('(?<=[.!?]) +', response) if s.strip()]
-        print("Split response into sentences.")
 
-        ### NO MORE REGEX - next part deals with any repeating sentences
         # Remove any repeating sentences from the response
         new_sentences = []
         for i, s in enumerate(sentences):
@@ -98,10 +93,19 @@ def format_response(prompt, response):
 
             if unique and not any(phrase in s.lower() for phrase in omit_phrases):
                 if s[:-1] not in prompt:
-                    # Append "!!!" to the last sentence - Emily style
-                    if i == len(sentences) - 1:
-                        s = s[:-1] + "!!!"
                     new_sentences.append(s)
+
+        # If last sentence is incomplete (i.e., doesn't end with a proper punctuation), remove it
+        if len(new_sentences) > 0:
+            last_sentence = new_sentences[-1]
+            if last_sentence[-1] not in {'.', '!', '?'}:
+                new_sentences = new_sentences[:-1]
+
+        # If there's still any sentence left, replace the punctuation of the last sentence with "!!!"
+        if len(new_sentences) > 0:
+            last_sentence = new_sentences[-1]
+            if last_sentence[-1] in {'.', '!', '?'}:
+                new_sentences[-1] = last_sentence[:-1] + "!!!"
 
         # Join sentences
         response = " ".join(new_sentences)
@@ -110,11 +114,17 @@ def format_response(prompt, response):
     return response
 
 
+
 # Assigns a similarity score to use with the generated response to remove repeating words/sentences
 def similarity_score(sentence1, sentence2):
+    # If either of the sentences is empty, return 0
+    if not sentence1.strip() or not sentence2.strip():
+        return 0
+
     vectorizer = TfidfVectorizer().fit_transform([sentence1, sentence2])
     cosine_similarities = cosine_similarity(vectorizer)
     return cosine_similarities[0, 1]
+
 
 
 # "Main" response function - called from handler file
