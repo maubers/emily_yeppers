b26577888425819a5f89000884be45ddeedc4ce1
maubers <126840418+maubers@users.noreply.github.com>
2023-07-05 13:52:01 -0400

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -5,26 +5,27 @@ A config file of sorts.
 '''
 
 import os
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 import re
 
 # Load GPT-2 model and tokenizer
-model_path = "emily_yeppers_4.0"
-tokenizer = GPT2Tokenizer.from_pretrained(model_path)
+model_path = "maubers/emily_yeppers"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.pad_token_id = tokenizer.eos_token_id
-model = GPT2LMHeadModel.from_pretrained(model_path, pad_token_id=tokenizer.pad_token_id)
+model = AutoModelForCausalLM.from_pretrained(model_path, pad_token_id=tokenizer.pad_token_id)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-crazy_factor = 1.3      # Keep high if you want her to act crazy
+crazy_factor = 1.3     # Keep high if you want her to act crazy
 response_length = 150   # She will rant about cum forever at a higher temperature if you don't keep this lower
 
 # Define list of words that neither prompts nor replies should contain
 unwanted_words = ["disney", "applebees", "scientology", "scientologist", "scientologists", "applebee's"]
 default_response = """
 # CUM WILL WIN
+
 """
 
 # Environment variables for authentication
@@ -39,8 +40,9 @@ password = os.environ["PASSWORD"]
 TARGET_PHRASES = ['emily yeppers', 'cum', 'olive juice']
 trigger_pattern = r'\b(' + '|'.join(TARGET_PHRASES) + r')\b'
 trigger_regex = re.compile(trigger_pattern, flags=re.IGNORECASE)
-subreddits = ['shittyaskreddit', 'testthewalrus', 'AssCredit']
+subreddits = ['testthewalrus', 'AssCredit', 'shittyaskreddit', 'Shittieraskreddit']
 bot_mention = "u/EMILY\_YEPPERS"
 shutup_mention = "shut the fuck up"
 omit_phrases = ["serious eats", "instagram", "affiliate", "subscribe", "@"]
+donotreply_users = ["EMILY_YEPPERS", "AutoModerator", "HippoBot9000", "radiantskie"]
 relationships = {}      # Use this dictionary to make the bot behave differently with specific users
